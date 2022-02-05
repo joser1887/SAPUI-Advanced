cc41bfe42747ce099b7d4d1466d38a08a4caa1e0
joser1887 <joser_1887@hotmail.com>
2022-02-05 22:55:27 +0000

    First Commit 04

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -6,10 +6,20 @@ sap.ui.define([
      */
     function (Controller) {
         "use strict";
-
-        return Controller.extend("ProyectN2.employees.controller.MainView", {
-            onInit: function () {
-
-            }
-        });
+        var Main = Controller.extend("ProyectN2.employees.controller.MainView", {});
+            
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmploye = inputEmployee.getValue();
+            if (valueEmploye.length === 6) {
+                //inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                //inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            };
+        };
+        return Main;
     });
